7fde0cb3c8f235aa6cf9a0dbc06c8aeba956680a
Alberto <dany.canaviri2022@gmail.com>
2026-06-19 22:13:54 -0400

    JavaScript for Show and Hidden the Sidebar, completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 /*=============== SHOW & HIDDEN ===============*/
+const sidebar = document.getElementById('sidebar'),
+      headerToggle = document.getElementById('header-toggle'),
+      sidebarClose = document.getElementById('sidebar-close')
 
+/* Show menu */
+if (headerToggle) {
+    headerToggle.addEventListener('click', () => {
+        sidebar.classList.add('show-sidebar')
+    })
+}
+
+/* Close menu */
+if (sidebarClose) {
+    sidebarClose.addEventListener('click', () => {
+        sidebar.classList.remove('show-sidebar')
+    })
+}
 
 /*=============== EXPAND & REDUCE ===============*/
